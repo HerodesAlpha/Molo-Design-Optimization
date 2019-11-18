5db0e91a24c582c474d49fb5c0d80344f0b4a5ec
Eivind Sønju <es@verbun.com>
2019-11-18 21:15:33 +0100

    Work on GUI contd'

--- Python/Single_candidate/main.py
+++ Python/Single_candidate/main.py
@@ -7,11 +7,6 @@ from tkinter import *
 from tkinter import filedialog
 import tkinter as tk
 
-
-
-
-
-
 import pygubu
 from plot_forces import get_axs
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -28,7 +23,7 @@ class MyApplication:
 
     def __init__(self):
         self.about_dialog = None
-        self.ax = None
+        self.fig = None
         self.builder = b = pygubu.Builder()
         b.add_from_file(os.path.join(DATA_DIR, 'main.ui'))
         b.add_resource_path(os.path.join(DATA_DIR, 'imgs'))
@@ -47,47 +42,37 @@ class MyApplication:
     def show_about_dialog(self):
         if self.about_dialog is None:
             dialog = self.builder.get_object('dlg_about', self.mainwindow)
-
             self.about_dialog = dialog
-
-
             def dialog_btnclose_clicked():
                 dialog.close()
-
             btnclose = self.builder.get_object('about_btnclose')
             btnclose['command'] = dialog_btnclose_clicked
-
             dialog.run()
         else:
             self.about_dialog.show()
 
-
-
     def quit(self, event=None):
         self.mainwindow.quit()
 
+    def btnplot_clicked(self):
+        if not self.fig==None:
+            self.fig.clear()
+        print('Plotting ...')
+        self.fig, self.axs = get_axs()
+        print('Fig retrieved')
+        self.fig.show()
+        master = self.builder.get_object('main_canvas')
+        canvas = FigureCanvasTkAgg(self.fig, master=master)
+        canvas.draw()
 
     def plot(self):
 
-        def btnplot_clicked():
-            if not self.ax==None:
-                self.ax.clear()
-            print('Plotting...')
-            self.fig, self.axs = get_axs()
-            master = self.builder.get_object('main_canvas')
-            self.canvas = FigureCanvasTkAgg(self.fig, master=master)
-            self.canvas.draw()
 
         btnplot = self.builder.get_object('plot_button')
-        btnplot['command'] = btnplot_clicked
-
-
-
-
+        btnplot['command'] = self.btnplot_clicked()
 
     def run(self):
         self.mainwindow.mainloop()
-
 
     def open_workspace(self, title=None, dirName=None):
         options = {}
