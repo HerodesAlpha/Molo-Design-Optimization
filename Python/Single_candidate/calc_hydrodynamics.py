b5b5040ef03d63af646bdd701de09393af6d552e
Eivind Sønju <es@verbun.com>
2020-01-04 21:33:17 +0100

    gui case config

--- Python/Single_candidate/calc_hydrodynamics.py
+++ Python/Single_candidate/calc_hydrodynamics.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
         this_candidate=pickle.load(f)
 
     this_candidate.settings.load_cases = {  # 121, np.pi / 15, np.pi
-            "num_wave_frequencies": 80,  # TODO: Implement adaptive frequency
+            "num_wave_frequencies": 120,  # TODO: Implement adaptive frequency
             "min_wave_frequencies": 2 * np.pi / 35,  # (rad/s)
-            "max_wave_frequencies": 2 * np.pi / 4,
+            "max_wave_frequencies": 2 * np.pi / 1,
             "num_wave_directions" : 1,
             "min_wave_directions" : 0,  # deg
             "max_wave_directions" : 90,
--- Python/Single_candidate/mdo_gui_main.py
+++ Python/Single_candidate/mdo_gui_main.py
@@ -6,7 +6,7 @@ import math
 from tkinter import *
 from tkinter import filedialog
 import tkinter as tk
-from pathlib import Path
+from pathlib import Path, PurePath
 import pickle
 from design_engine import Candidate, Parameter_Space
 import os
@@ -20,6 +20,10 @@ import tool_box as tb
 import json
 from tkinter import messagebox
 import numpy as np
+import environmental_conditions as ec
+import module_locator
+# my_path = module_locator.module_path()
+# print(my_path)
 
 try:
     DATA_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -59,8 +63,13 @@ class Application():
 
         b.connect_callbacks(self)
 
-        self.load_cfg()
+        self.case_cfg = None
+        self.global_cfg = None
         self.this_candidate=None
+        self.data_io_dir=None
+        self.pkl_path=None
+
+        self.load_cfg()
 
     def get_output(self, text_box):
         self.text_box = self.builder.get_object(text_box)
@@ -83,19 +92,38 @@ class Application():
 
     def save_cfg(self):
         # Save current settings
-        for id in self.cfg['user_input']['object']:
+        for id in self.global_cfg['user_input']['object']:
             value = self.builder.get_object(id).get()
-            self.cfg['user_input']['object'][id]['value'] = value
+            self.global_cfg['user_input']['object'][id]['value'] = value
 
-        with open('mdo_cfg.json', 'w') as f:
-            f.write(json.dumps(self.cfg, indent=4, sort_keys=True))
+        f_global = Path(DATA_DIR).joinpath('mdo_global_cfg.json')
+        with open(f_global, 'w') as f:
+            f.write(json.dumps(self.global_cfg, indent=4, sort_keys=True))
+
+        f_case = self.data_io_dir.joinpath('mdo_case_cfg.json')
+        with open(f_case, 'w') as f:
+            f.write(json.dumps(self.case_cfg, indent=4, sort_keys=True))
 
     def load_cfg(self):
-        # Read config
-        with open('mdo_cfg.json', 'r') as f:
-            self.cfg = json.loads(f.read())
+        # Read global config
+        f_global=Path(DATA_DIR).joinpath('mdo_global_cfg.json')
 
-        objects=self.cfg['user_input']['object']
+        with open(f_global, 'r') as f:
+            self.global_cfg = json.loads(f.read())
+
+        if  isinstance(self.data_io_dir, Path):
+            f_case = self.data_io_dir.joinpath('mdo_case_cfg.json')
+            if f_case.exists():
+                with open(str(f_case), 'r') as f:
+                    self.case_cfg = json.loads(f.read())
+        else:
+            f_case = Path(DATA_DIR).joinpath('mdo_default_case_cfg.json')
+            if f_case.exists():
+                with open(f_case, 'r') as f:
+                    self.case_cfg = json.loads(f.read())
+
+        objects=self.global_cfg['user_input']['object']
+        objects.update(self.case_cfg['user_input']['object'])
         for id in objects:
                 value = objects[id]['value']
                 try:
@@ -135,11 +163,11 @@ class Application():
     def create_model(self):
         self.get_output('model_text')
 
-        analyses_root = Path(self.builder.get_object('analyses_root_input').get())
-        print(analyses_root)
+        analyses_root_input = Path(self.builder.get_object('analyses_root_input').get())
+        print(analyses_root_input)
 
-        park_label = self.builder.get_object('park_label_input').get()
-        wtg_label = self.builder.get_object('wtg_label_input').get()
+        park_label_input = self.builder.get_object('park_label_input').get()
+        wtg_label_input = self.builder.get_object('wtg_label_input').get()
         template_dir = Path(os.getcwd()).parents[0].joinpath('Optimization').joinpath('templates')
 
         p = Parameter_Space(templates_dir=template_dir)
@@ -156,17 +184,21 @@ class Application():
         print('Gap factor: {:6.3f}'.format(p.gap))
 
         p.filling_ratio = [0.1, 0.1, 0.1]
-        this_candidate = Candidate(analyses_root, park_label, wtg_label, p, case_label_type='molo_model')
-        this_candidate.settings.wtg_model = "Vestas 9.5"
-        this_candidate.init_model(state='New')
-        with open("this_candidate.pkl", "wb") as f:
-            pickle.dump(this_candidate, f)
+        self.this_candidate = Candidate(analyses_root_input, park_label_input, wtg_label_input, p, case_label_type='molo_model')
+        self.this_candidate.settings.wtg_model = "Vestas 9.5"
+        self.this_candidate.init_model(state='New')
+        self.data_io_dir=self.this_candidate.settings.fio.data_io_dir
+        self.pkl_path=self.data_io_dir.joinpath('this_candidate.pkl')
+        with open(self.pkl_path, "wb") as f:
+            pickle.dump(self.this_candidate, f)
 
     def calc_stability(self):
 
         self.get_output('stability_text')
 
-        with open("this_candidate.pkl", "rb") as f:
+
+
+        with open(self.pkl_path, "rb") as f:
             self.this_candidate = pickle.load(f)
 
         self.this_candidate.settings.create_stability_movie = self.builder.get_variable('stability_movie_chkbtn_var')
@@ -184,7 +216,7 @@ class Application():
     def run_hydro(self):
         self.get_output('hydro_text')
 
-        with open("this_candidate.pkl", "rb") as f:
+        with open(self.pkl_path, "rb") as f:
             self.this_candidate = pickle.load(f)
 
         self.this_candidate.settings.load_cases = {
@@ -196,10 +228,9 @@ class Application():
                 "max_wave_directions" : self.get_numeric('max_wave_directions_input'),
         }
 
-
-
-
         self.this_candidate.hydrodynamic_analysis()
+
+
 
     def open_workspace(self, title=None, dirName=None):
         options = {}
@@ -212,6 +243,64 @@ class Application():
         else:
             return fileName
 
+    def calc_response(self):
+        self.get_output('response_text')
+        tc = self.this_candidate
+
+        print('\nCase:\t{}'.format(self.this_candidate.settings.case_label))
+
+        force_label = ['Fx [MN]', 'Fy [MN]', 'Fz [MN]', 'Mx [MNm]', 'My [MNm]', 'Mz [MNm]']
+
+        yr = self.this_candidate.settings.park_data['Design Basis']['ULS']['Return period']
+
+        # Create list of short terms from contour line
+        #area = this_candidate.settings.park_data['Design Basis']['Area']
+        area = 9
+        tc.ltwc1 = ec.Long_Term_Wave_Conditions(area=area)
+        tc.cl = tc.ltwc1.contour_line(27)
+        tc.contourline = []
+        #this_candidate.cl = np.asarray([[3.5, 13.5, 1],[9, 9.5, 5],[8, 11, 3.6],[7, 11, 2.6],[7, 11.5, 2.12]])
+
+        for hs, tz, in tc.cl:
+            #print(' {:6.1f} {:6.1f}'.format(hs, tz))
+            tc.contourline.append(ec.Short_Term_Wave_Conditions(hs=hs, tz=tz, gamma=None))
+
+        tc.settings.radiaton_damping_factor = 1
+
+
+
+        tc.settings.do_linearize=True
+        tc.init_load()
+        ibeta = 0
+        for stwc in tc.contourline:
+            tc.init_response(short_term_wave_condition=stwc)
+
+            # Get section forces
+            sp_x = tc.settings.floater_data['Central column diameter'] * (0.5)  # + 0.8 + 1 + 0.8 + 1)
+            sp_z = tc.settings.floater_data['Radial']['Heigth'] / 2 - tc.hs_floater.hs_data[
+                'draught']
+            # sp_x = -100
+            sp_z = 0
+            section_point = [sp_x, 0, sp_z]  # Used for moment reference
+            section_normal = [1, 0, 0]
+
+            imass, ipanel, istrip = tc.response.get_section_index(section_point, section_normal)
+            tc.f_sec1 = tc.response.assemble_forces(imass, ipanel, istrip, moment_ref_point=[0, 0, 0])
+            del imass, ipanel
+
+
+
+
+            fz = tc.f_sec1['Dynamic']['SUM'][:, ibeta, 2] / 1000000
+            my = tc.f_sec1['Dynamic']['SUM'][:, ibeta, 4] / 1000000
+
+
+
+            fz_elm=stwc.expected_largest_maximum(fz, tc.loads.w)
+            my_elm=stwc.expected_largest_maximum(my, tc.loads.w)
+
+
+            print(' {:6.1f} {:6.1f} {:6.1f}  {:6.2f}  {:6.1f} '.format(stwc.hs, stwc.tp, stwc.gamma, fz_elm, my_elm))
 
 if __name__ == '__main__':
     # root = tk.Tk()
