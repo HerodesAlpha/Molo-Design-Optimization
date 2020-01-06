e51d0e25f19e77fe7d3968d88077b1d7d95a71d5
Eivind Sønju <es@verbun.com>
2020-01-06 16:12:11 +0100

    Auto ballast calc.

--- Python/core/model_set_up.py
+++ Python/core/model_set_up.py
@@ -26,113 +26,6 @@ def rem_list(df):
     return df
 
 
-def read_excel_model(excel_file, floater_id, tower_id, rna_id):
-    fdst = 'tmp.xlsx'
-    # shutil.copy(excel_file,fdst)
-    cmd = 'xcopy /r /y {} {}'.format(excel_file, fdst)
-    try:
-        os.system(cmd)
-    except OSError:
-        # File most likely does not exist
-        print("File most likely does not exist")
-        exit()
-
-    df_floater = pd.read_excel(fdst, sheet_name='Floater', index=False)
-    df_rna = pd.read_excel(fdst, sheet_name='RNA', index=False)
-    df_tower = pd.read_excel(fdst, sheet_name='Tower', index=False)
-    df_b2c = pd.read_excel(fdst, sheet_name='Ballast_2C', index=False)
-    df_b3c = pd.read_excel(fdst, sheet_name='Ballast_3C', index=False)
-    df_b4c = pd.read_excel(fdst, sheet_name='Ballast_4C', index=False)
-
-    a = df_floater[df_floater.ID == floater_id].to_dict(orient='list')
-
-    dict_tower_data = rem_list(df_tower[df_tower.ID == tower_id].to_dict(orient='list'))
-    dict_floater_data = rem_list(df_floater[df_floater.ID == floater_id].to_dict(orient='list'))
-    dict_rna_data = rem_list(rem_list(df_rna[df_rna.ID == rna_id].to_dict(orient='list')))
-
-    twr_data = model.TowerDataClass(dict_tower_data)
-    floater_data = model.FloaterDataClass(dict_floater_data)
-    rna_data = model.RNADataClass(dict_rna_data)
-
-    # Set global z
-    interface_point = floater_data.t_lf + floater_data.hgt + floater_data.t_uf
-    twr_data.p = [0, 0, -(interface_point + twr_data.h / 2)]
-    rna_data.p = [0, 0, -(interface_point + twr_data.h)]
-    # Write json
-    # with open('model_template.json', 'w') as f:
-    #     f.write(json.dumps({'twr': dict_tower_data, 'rna': dict_rna_data, 'floater': dict_floater_data,'test':[[1,0,1],[1,0,1]]}, indent=4,
-    #                        sort_keys=True))
-
-    rho_st = 7850
-    #
-    # Build floater
-    floater_model = model.FloaterClass(floater_data, rho_st)  # CoGz will be set for BOS at z=0
-
-    # Build tower and RNA
-    wtg_model = model.WtgClass(twr_data, rna_data, rho_st)  # CoGz will be controlled by p
-
-    # Assemble parts into complete unit
-    unit_model = model.UnitClass([wtg_model, floater_model])
-    return unit_model, floater_model, wtg_model
-
-
-def model01():
-    rna_mass = 500000
-    turbine_diameter = 167
-    hub_height_above_interface_point = turbine_diameter / 2 + 30
-
-    fdi = dict()
-    fdi['type'] = 'MOLO'
-    fdi['nc'] = 4  # 2, 3 or 4
-    fdi['gap'] = 0.8
-    fdi['dia_rc'] = 7
-    fdi['thi_rc'] = 0.05
-    fdi['dia_hc'] = 6
-    fdi['thi_hc'] = 0.04
-    fdi['hgt'] = 14
-    fdi['t_lf'] = 0.04
-    fdi['t_uf'] = 0.04
-    fdi['ballast'] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    fdi['rho_bal'] = 1025
-    floater_data = model.FloaterDataClass(fdi)
-
-    interface_point = floater_data.t_lf + floater_data.hgt + floater_data.t_uf
-
-    tdi = dict()
-    tdi['type'] = 'Tower'
-    tdi['h'] = (hub_height_above_interface_point / 2 + 35)
-    tdi['t'] = 0.05
-    tdi['d'] = 6
-    tdi['p'] = [0, 0, - (interface_point + hub_height_above_interface_point / 2)]
-
-    rdi = dict()
-    rdi['type'] = 'Vestas 9.5MW'
-    rdi['d_rot'] = turbine_diameter
-    rdi['m'] = rna_mass
-    rdi['ixx'] = 5 ** 2 * rna_mass
-    rdi['iyy'] = 10 ** 2 * rna_mass
-    rdi['izz'] = 10 ** 2 * rna_mass
-    rdi['iyz'] = 0
-    rdi['ixz'] = 0
-    rdi['ixy'] = 0
-    rdi['p'] = [0, 0, - (interface_point + hub_height_above_interface_point)]
-
-    rho_st = 7850
-    #
-    # Build floater
-    floater_data = model.FloaterDataClass(fdi)
-    floater_model = model.FloaterClass(floater_data, rho_st)  # CoGz will be set for BOS at z=0
-
-    # Build tower and RNA
-    twr_data = model.TowerDataClass(tdi)
-    rna_data = model.RNADataClass(rdi)
-    wtg_model = model.WtgClass(twr_data, rna_data, rho_st)  # CoGz will be controlled by p
-
-    # Assemble parts into complete unit
-    unit_model = model.UnitClass([wtg_model, floater_model])
-    return unit_model, floater_model, wtg_model
-
-
 def create_mass_models(settings):
     twr_data = model.TowerDataClass(settings.job_data['wtg'][settings.wtg_model]["Tower"])
     floater_data = model.FloaterDataClass(settings.job_data['floater'])
@@ -144,8 +37,6 @@ def create_mass_models(settings):
     rna_data.p = [0, 0, -(interface_point + twr_data.h)]
 
     # Build floater
-    # Set ballast to zero
-    settings.job_data['floater'] = {"Ballast filling ratio": [0.0, [0.0, 0.0, 0.0, 0.0]]}
 
     floater_model = model.FloaterClass(floater_data, settings)  # CoGz will be set for BOS at z=0
 
@@ -155,32 +46,14 @@ def create_mass_models(settings):
     # Assemble parts into complete unit
     unit_model = model.UnitClass([wtg_model, floater_model])
 
-    # Now ballast to unit depth
-
-    rho_ball = settings.job_data['floater']['Ballast density']
-    rhos_sw = settings.rho_sw
-    m_unit = unit_model.mass
-    d_rc = floater_data.dia_rc
-    d_hc = floater_data.dia_hc
-    nr = 3
-    nc = floater_data.nc
-    a_wp = np.pi * (d_hc ** 2 + nr * nc * d_rc ** 2) / 4
-    h = floater_data.hgt
-    m_ball = h / 2 * a_wp * rhos_sw - m_unit
-    fr = (m_ball / (a_wp * rho_ball)) / h
-    print('Required ballast to {:1.2f} m is {:1.1f} ton\n Filling ratio is {:1.2f}'.format(h / 2, m_ball / 1000,fr))
-    settings.job_data['floater'] = {"Ballast filling ratio": [fr, [fr, fr, fr, fr]]}
-
-    #Rebuild unit with ballast
-    floater_model = model.FloaterClass(floater_data, settings)  # CoGz will be set for BOS at z=0
-    unit_model = model.UnitClass([wtg_model, floater_model])
-
-
-    return unit_model, floater_model, wtg_model
+    return unit_model, floater_model, wtg_model, floater_data
 
 
 def init_models(settings):  #
-    unit_model, floater_model, wtg_model = create_mass_models(settings)
+    # Set ballast to zero
+    nc=settings.job_data['floater']['Radial']['Number of columns']
+    settings.job_data['floater']['Ballast filling ratio'] = [0.0, [0.0 for i in range(nc)]]
+    unit_model, floater_model, wtg_model, floater_data = create_mass_models(settings)
 
     for part in unit_model.parts_list:
         part.print_vector_matrix_global()
@@ -200,26 +73,41 @@ def init_models(settings):  #
     hs_floater.gravity = abs(settings.gravity)
     hs_floater.rho_water = settings.rho_sw
     hs_floater.mass = unit_model.mass / 1000  # Give mass in tons
-    print('\nMass given to hydro is {:5.2f} t'.format(hs_floater.mass))
+    print('\nUn-ballasted mass given to hydro is {:5.2f} t'.format(hs_floater.mass))
     hs_floater.gravity_center = -unit_model.inertias.reduction_point
     # TODO: THIS IS TEMP SPEEDUP. FIX IT !!
     # if settings.do_equilibrate:
     #     hs_floater.equilibrate()
     hs_floater.set_displacement(hs_floater.mass)
+    print('Un-ballasted draught is {:1.2f} m'.format(hs_floater.hs_data['draught']))
 
-    #
+    d_rc = floater_data.dia_rc
+    d_hc = floater_data.dia_hc
+    nr = 3
+    nc = floater_data.nc
+    a_wp = np.pi * (d_hc ** 2 + nr * nc * d_rc ** 2) / 4
+    floater_hgt = floater_data.hgt
+    m_ball = (floater_data.hgt / 2 - hs_floater.hs_data['draught']) * a_wp * settings.rho_sw
+    fr = (m_ball / (a_wp * settings.job_data['floater']['Ballast density'])) / floater_hgt
+    print(
+        '\nRequired ballast to {:1.2f} m is {:1.1f} ton\nFilling ratio is {:1.2f}'.format(floater_hgt / 2, m_ball / 1000,
+                                                                                         fr))
+    settings.job_data['floater']['Ballast filling ratio'] = [fr, [fr for i in range(nc)]]
+    print("\nRecreate mass model with target ballast")
+    unit_model, floater_model, wtg_model, floater_data = create_mass_models(settings)
+    hs_floater.mass = unit_model.mass / 1000  # Give mass in tons
+    print('\nBallasted mass given to hydro is {:5.2f} t'.format(hs_floater.mass))
+    hs_floater.gravity_center = -unit_model.inertias.reduction_point
+    hs_floater.set_displacement(hs_floater.mass)
+
+
+    settings.draught = hs_floater.hs_data['draught']
+    print('\nRecreate mass model for ballasted draught = {:5.2f}m'.format(settings.draught))
+    unit_model, floater_model, wtg_model, floater_data = create_mass_models(settings)
+
     # hs_floater.show()
     print(hs_floater.get_hydrostatic_report())
     settings._report.write_hydrostatic_report_latex_table(hs_floater)
-    # Update model with calculated draft
-    # print('\nMass matrix just before adjusting to draught')
-    # for part in unit_model.get_all_parts():
-    #     part.print_vector_matrix_global()
-    # unit_model.move_reduction_point([0, 0, hs_floater.hs_data['draught']])
-
-    settings.draught = hs_floater.hs_data['draught']
-    print('Recreate mass model for new draught = {:5.2f}m'.format(settings.draught))
-    unit_model, floater_model, wtg_model = create_mass_models(settings)
 
     tb.save_M_and_K(settings, M=unit_model.inertias.mass_matrix_global,
                     MMK=hs_floater.hs_data['stiffness_matrix'])
